54b87e7c5ee98ec125e63307c0f75d3ed0b7c808
guasin <alancarrizo1017@gmail.com>
2022-09-01 20:29:32 -0300

    modifico dados

--- dado.py
+++ dado.py
@@ -5,9 +5,18 @@ class dado():
         self.caras=caras
 
     def tirar(self):
-        print("Salió ",random.randint(1,self.caras))
-        cara = random.randint(1,self.caras)
-        return cara
-        
+        cara1 = random.randint(1,self.caras)
+        print(f"\nSalió {cara1}")
+    
+    def varios_dados(self,cantidad_dados):
+        total=0
+        for i in range (0,cantidad_dados):
+            cara = random.randint(1,self.caras)
+            total += cara
+            print ("Salió ", cara)
+        print ("\nEl total es: ",total)
+        return total
+
 dado1=dado(6)
+dado1.varios_dados(9)
 dado1.tirar()
